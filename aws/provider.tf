fd3f6d686bf81359115f55edca3386b4415493b4
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2023-01-27 11:45:17 +0000

    Update provider.tf

--- aws/provider.tf
+++ aws/provider.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-    access_key = "AKIAWMZONI2W5KT5JPHA"
-    secret_key = "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"
+    access_key = "xxxxxxxxxxxxxxxxxxxxx"
+    secret_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx9"
    
 } 
